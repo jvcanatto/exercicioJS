3f9b7fb8908ad117429352368321c09d573a310c
JOAO VICTOR CANATTO DE ARAUJO <40554692848@info.local>
2024-05-09 08:53:31 -0300

    feat: exercicio 2

---
+++ ex2.js
@@ -0,0 +1,34 @@
+/*Exercício 2: Verificação de dia da semana
+Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+2 representa terça-feira e assim por diante. */
+
+let dia = prompt ("Dias da semana")
+
+if (dia == 1) {
+    console.log ("Hoje é segunda-feira!")
+}
+
+else if (dia == 2) {
+    console.log ("Hoje é terça-feira!")
+}
+
+else if (dia == 3) {
+    console.log ("Hoje é quarta-feira!")
+}
+
+else if (dia == 4) {
+    console.log ("Hoje é quinta-feira!")
+}
+
+else if (dia == 5) {
+    console.log ("Hoje é sexta-feira!")
+}
+
+else if (dia == 6) {
+    console.log ("Hoje é sábado!")
+}
+
+else if (dia == 7) {
+    console.log ("Hoje é domingo!")
+}
