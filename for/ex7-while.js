7d3e6671746f2077e242c19cce0a54fdfee3b7e8
JOAO VICTOR CANATTO DE ARAUJO <40554692848@info.local>
2024-05-14 08:48:42 -0300

    feat: exercicio 7

---
+++ for/ex7-while.js
@@ -0,0 +1,12 @@
+/* 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+while. */
+
+let numero = 1;
+let soma = 0;
+
+while (numero <= 100) {
+    soma += numero;
+    numero++;
+}
+
+console.log("a soma dos números de 1 a 100 são:", soma);
